3c1aeabf52ab35712e8bcd32279c2ee26d11e6f3
Javier Avitia <Javier.A.Avitia@gmail.com>
2017-08-04 08:30:24 -0700

    No major changes, but pushing for complete record.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -45,13 +45,53 @@ $( ".btn" ).click(function(event) {
 
 //moment("123", "hmm").format("HH:mm")
 
+/*
+
+if (parseInt(minutes) >= 0){
+
+			console.log("In here!")
+
+			minutes = moment(childSnap.val().firstTime.replace(":",""), "Hmm").diff(moment(),'minutes');
+
+			nextTrain = childSnap.val().firstTime;
+
+		}
+		else{
+
+			minutes = moment().diff(moment(childSnap.val().firstTime.replace(":",""), "Hmm"),'minutes');
+
+			minutes = minutes % childSnap.val().fMin;
+
+			minutes = childSnap.val().fMin - minutes;
+
+			nextTrain = moment().add((minutes % childSnap.val().fMin),"minutes").format("HH:mm");
+
+		}
+
+*/
+
+/*
+setInterval(function(){
+
+				var time = new moment();
+
+				console.log(time)
+
+				//var difference = moment(childSnap.val().firstTime.replace(":",""), "Hmm").diff(time,'minutes');
+				$('tr').find('td').each (function() {
+				  	console.log($('td').text() + "test");
+				});  
+
+
+},3000);*/ //Could not get to log properly
+
 
 database.ref("/choochoo").on("child_added",function(childSnap){
 
 		//converting childSnap.val().firstTime into "Hmm" formatted moment to compare to moment() [current time] and find .diff
 		//var minutes = moment().diff(moment(childSnap.val().firstTime.replace(":",""), "Hmm"),'minutes');
 
-		console.log(moment().format("HH:mm") + "; " + current_time.format("HH:mm"))
+		//console.log(moment().format("HH:mm") + "; " + current_time.format("HH:mm"))
 		
 		var difference = moment(childSnap.val().firstTime.replace(":",""), "Hmm").diff(current_time,'minutes');
 		var minutes;
@@ -61,7 +101,7 @@ database.ref("/choochoo").on("child_added",function(childSnap){
 
 			minutes = difference;
 
-			nextTrain = childSnap.val().firstTime;
+			nextTrain = moment(childSnap.val().firstTime.replace(":",""), "Hmm").format("hh:mm a");
 
 		}
 		else{
@@ -72,19 +112,19 @@ database.ref("/choochoo").on("child_added",function(childSnap){
 
 			minutes = childSnap.val().fMin - minutes;
 
-			nextTrain = current_time.add(minutes,"minutes").format("HH:mm");
+			nextTrain = current_time.add(minutes,"minutes").format("hh:mm a");
 
-			//console.log(moment().format("HH:mm") + " " + minutes)
-			current_time.subtract(minutes,"minutes").format("HH:mm");
+			//console.log(moment().format("hh:mm a") + " " + minutes)
+			current_time.subtract(minutes,"minutes").format("hh:mm a");
 
 		}
 
 		var new_row = $("<tr>");
 		new_row.append("<td>" + childSnap.val().trainName + "</td>"
 		+ "<td>" + childSnap.val().dest + "</td>"
-		+ "<td>" + childSnap.val().fMin + "</td>"
-		+ "<td>" + nextTrain + "</td>"
-		+ "<td>" + minutes + "</td>");
+		+ "<td id='freq'>" + childSnap.val().fMin + "</td>"
+		+ "<td id='next'>" + nextTrain + "</td>"
+		+ "<td id='minutes'>" + minutes + "</td>");
 
 		$("tbody").append(new_row);
 
